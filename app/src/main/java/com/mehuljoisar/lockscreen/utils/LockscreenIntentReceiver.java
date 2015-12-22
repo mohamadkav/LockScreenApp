c627fab9f46252bc14bf45e6a2e6a489988831a8
Mohammad <mohamad_kav@yahoo.co.uk>
2015-12-22 13:53:58 +0330

    Fixed bug when not giving the pattern

--- app/src/main/java/com/mehuljoisar/lockscreen/utils/LockscreenIntentReceiver.java
+++ app/src/main/java/com/mehuljoisar/lockscreen/utils/LockscreenIntentReceiver.java
@@ -44,6 +44,10 @@ public class LockscreenIntentReceiver extends BroadcastReceiver {
 			if(mTopView!=null)
 				return;
 			final WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+            if(patternViewGroup!=null){
+                wm.removeViewImmediate(patternViewGroup);
+                patternViewGroup=null;
+            }
 			WindowManager.LayoutParams params = new WindowManager.LayoutParams(
 					WindowManager.LayoutParams.TYPE_SYSTEM_ALERT,//OVERLAY,
 					WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH |
@@ -79,14 +83,6 @@ public class LockscreenIntentReceiver extends BroadcastReceiver {
 					mTopView = null;
 				}
 			});
-            if(Build.VERSION.SDK_INT>=19)
-            mTopView.setSystemUiVisibility(
-                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
             wm.addView(patternViewGroup,params);
 			wm.addView(mTopView, params);
 		}catch (Exception e) {
